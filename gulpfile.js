41626e088b187a64213535d131324684f2a4b528
andrey <andreytorrente@gmail.com>
2022-01-27 23:27:39 -0600

    add uglify to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
-const {src, dest, watch} = require('gulp');
-const sass = require ('gulp-sass')(require('sass'));
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const uglify = require("gulp-uglify");
 
-
-function compile(){
-    return src('./app/sass/**/*.scss')
-    .pipe(sass({
-        outputStyle: 'compressed'
-   }))
-    .pipe(dest('./css'));
+function compile() {
+  return src("./src/sass/**/*.scss")
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+      })
+    )
+    .pipe(dest("./public/css"));
 }
 
-
-function watchSass(){
-    watch('./app/sass/**/*.scss',compile);
+function watchSass() {
+  watch("./src/sass/**/*.scss", compile);
+}
+function uglifyjs() {
+  return src("./src/js/*.js")
+  .pipe(uglify())
+  .pipe(dest("./public/js"))
 }
 
 exports.compile = compile;
 exports.watchSass = watchSass;
+exports.uglifyjs = uglifyjs;
